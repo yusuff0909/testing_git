200cea7f5999839f434d799edc15e36de91b61cf
Yusuff <steve.elegbede@gmail.com>
2024-06-28 18:24:27 -0400

    new changes

---
+++ main.tf
@@ -0,0 +1,13 @@
+resource "aws_lightsail_instance" "custom" {
+  name              = "Myserver"
+  availability_zone = "us-east-1b"
+  blueprint_id      = "amazon_linux_2"
+  bundle_id         = "nano_3_0"
+  user_data         = file("script.sh")
+  key_pair_name     = "week2"
+  tags = {
+    Team       = "devops"
+    env        = "dev"
+    created_by = "terraform"
+  }
+}
