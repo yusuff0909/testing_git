b58a4dfcca8318413ff22f2fbf72221eb5c9c867
Yusuff <steve.elegbede@gmail.com>
2024-06-28 18:40:25 -0400

    nokeypair

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "aws_lightsail_instance" "custom" {
   blueprint_id      = "amazon_linux_2"
   bundle_id         = "nano_3_0"
   user_data         = file("script.sh")
-  key_pair_name     = "week2"
+  #key_pair_name     = ""
   tags = {
     Team       = "devops"
     env        = "dev"
